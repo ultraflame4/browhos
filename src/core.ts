9627a5b33caa6ac4a9d1ecb2f98ab6eb59358be6
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-11-29 18:00:52 +0800

    Added ability to go straight to urls

--- src/core.ts
+++ src/core.ts
@@ -1,10 +1,23 @@
 const googleSearch = "https://www.google.com/search"
 
 
+const urlTest = new RegExp(/(^(http|file|https)\:\/\/)?(([a-z0-9][a-z0-9\-_~\/:\?#\[\]@!$&\'\(\)\*+,;=]*)(\.[a-z0-9\-_~\/:\?#\[\]@!$&\'\(\)\*+,;=])+|(\/$))/g)
+const protocolTest = new RegExp(/(^(http|file|https)\:\/\/)/g)
 
 function searchWithSearchEngine(s: string) {
+
+    if (urlTest.test(s)) {
+        if (protocolTest.test(s)) {
+
+            location.assign(s)
+        } else {
+            location.assign("https://" + s)
+        }
+        return
+    }
+
     const url = new URL(googleSearch)
-    url.searchParams.append("q",s)
+    url.searchParams.append("q", s)
     location.assign(url)
 }
 
