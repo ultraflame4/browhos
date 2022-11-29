153f98daec9fed2f47d3ec0f695388c1550424db
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-11-29 20:11:27 +0800

    Added rainbow time

---
+++ src/components/TimeBanner/TimeBanner.tsx
@@ -0,0 +1,24 @@
+import {defineComponent} from "@/utils";
+import {useEffect, useState} from "react";
+import classes from "./TimeBanner.module.css";
+
+
+export default defineComponent((props, context) => {
+    const [time,setTime] = useState("There is no time here. We are outside the space time continuum")
+
+    useEffect(() => {
+
+        const intervalIndex = setInterval(()=>{
+            let now  = new Date()
+            setTime(`${now.getHours()}:${now.getMinutes()}`)
+        },0)
+
+        return ()=>{
+            clearInterval(intervalIndex)
+        }
+    })
+
+    return (<h1 className={classes.timebanner}>
+        {time}
+    </h1>)
+})
